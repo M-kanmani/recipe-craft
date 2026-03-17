e1e9ce67688fbbe0034551ecd01daae5bc49fa6c
M-kanmani <kanmanimurugesan1526@gmail.com>
2026-03-17 21:39:29 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,4 @@
-import Header from "./Components/Header"
-import MainPage from "./Components/MainPage"
-// Main App component that renders the Header and MainPage components
 function App() {
-  // Render the main application with Header and MainPage
-  return (
-    <>
-      <Header/>
-      <MainPage/>
-    </>
-  )
+  return <h1>Working 🔥</h1>;
 }
-// Export the App component as default
-export default App
+export default App;
